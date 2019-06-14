b2d166c797e78e4aa272a7ad62a15e43eaf68ad2
zeroww <dev.zeroww@gmail.com>
2019-06-14 23:03:14 +0200

    feat(app): :sparkles: adds ngrx/storeFreeze, throws exeption when mutation occurs
    undefined

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -1,4 +1,5 @@
 import { ActionReducerMap, MetaReducer } from "@ngrx/store";
+import { storeFreeze } from "ngrx-store-freeze";
 
 import { environment } from "../../environments/environment";
 
@@ -7,5 +8,5 @@ export interface AppState {}
 export const reducers: ActionReducerMap<AppState> = {};
 
 export const metaReducers: MetaReducer<AppState>[] = !environment.production
-  ? []
+  ? [storeFreeze]
   : [];
