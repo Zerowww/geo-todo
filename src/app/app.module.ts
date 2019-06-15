1524339973f39627e3bcddd817ebb70342c969e8
zeroww <dev.zeroww@gmail.com>
2019-06-15 11:19:21 +0200

    refactor(app): :rocket: renamed to app.routing.ts
    undefined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,22 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {StoreModule} from '@ngrx/store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { StoreModule } from "@ngrx/store";
-import { reducers, metaReducers } from "./reducers";
-import { StoreDevtoolsModule } from "@ngrx/store-devtools";
-import { environment } from "../environments/environment";
+import {environment} from '../environments/environment';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app.routing';
+import {metaReducers, reducers} from './reducers';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    StoreModule.forRoot(reducers, { metaReducers }),
-    !environment.production ? StoreDevtoolsModule.instrument() : []
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [AppComponent],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		StoreModule.forRoot(reducers, {metaReducers}),
+		!environment.production ? StoreDevtoolsModule.instrument() : [],
+	],
+	providers: [],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
