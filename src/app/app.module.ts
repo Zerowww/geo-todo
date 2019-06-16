2c5e1abfd66ca1af95801b9cfc2ef0063c472e48
zeroww <dev.zeroww@gmail.com>
2019-06-16 09:28:42 +0200

    feat(app): :sparkles: added imports related to NgRx and mock banck-end
    undefined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,36 @@
 import {NgModule} from '@angular/core';
+import {EffectsModule} from '@ngrx/effects';
 import {StoreModule} from '@ngrx/store';
 import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
 
 import {environment} from '../environments/environment';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
 import {CoreModule} from './core/core.module';
+import {MockTodos} from './core/mock/mock-todos';
 import {metaReducers, reducers} from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    CoreModule,
     StoreModule.forRoot(reducers, {metaReducers}),
-    !environment.production ? StoreDevtoolsModule.instrument() : [],
+    EffectsModule.forRoot([]),
+    !environment.production
+      ? StoreDevtoolsModule.instrument({
+          name: 'NgRx Geo Store Devtools',
+          logOnly: environment.production,
+        })
+      : [],
+    environment.useMockData
+      ? HttpClientInMemoryWebApiModule.forRoot(MockTodos, {
+          delay: 1000,
+          post204: false,
+          put204: false,
+        })
+      : [],
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
