8ac035d2f013089bde47d74799601f5e76005246
zeroww <dev.zeroww@gmail.com>
2019-06-16 17:59:48 +0200

    feat(material): :sparkles: added some modules
    undefined

--- src/app/shared/material/material.module.ts
+++ src/app/shared/material/material.module.ts
@@ -1,10 +1,30 @@
-import {NgModule} from '@angular/core';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatListModule} from '@angular/material/list';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
-  imports: [MatListModule, MatButtonModule, MatCheckboxModule],
-  exports: [MatListModule, MatButtonModule, MatCheckboxModule],
+  imports: [
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+  ],
+  exports: [
+    MatListModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+  ],
 })
 export class MaterialModule {}
