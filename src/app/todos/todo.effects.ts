38eddc7f6fbaed17be93967336a841efbdbb3912
zeroww <dev.zeroww@gmail.com>
2019-06-16 14:43:23 +0200

    feat(todo-effects): :sparkles: added updateTodo effect
    undefined

--- src/app/todos/todo.effects.ts
+++ src/app/todos/todo.effects.ts
@@ -1,24 +1,46 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { select, Store } from '@ngrx/store';
-import { filter, map, mergeMap, withLatestFrom } from 'rxjs/operators';
+import { Action, select, Store } from '@ngrx/store';
+import { Observable, of } from 'rxjs';
+import { catchError, filter, map, mergeMap, switchMap, withLatestFrom } from 'rxjs/operators';
 
 import { AppState } from '../reducers';
+import { Todo } from '../shared/models/todo.model';
 import { TodosService } from '../shared/services/todos.service';
-import { LoadTodosSuccess, TodoActionTypes, TodosRequested } from './todo.actions';
-import { selectAllTodosLoaded } from './todo.selectors';
+import {
+  LoadTodosFailure,
+  LoadTodosSuccess,
+  TodoActionTypes,
+  TodosRequested,
+  UpdateTodo,
+  UpdateTodoFailure,
+  UpdateTodoSuccess,
+} from './todo.actions';
+import { selectAllTodosLoaded, selectTodoById } from './todo.selectors';
 
 @Injectable()
 export class TodoEffects {
 
   @Effect()
-  loadTodos$ = this.actions$.pipe(
+  loadTodos$: Observable<Action> = this.actions$.pipe(
     ofType<TodosRequested>(TodoActionTypes.TodosRequested),
     withLatestFrom(this.store.pipe(select(selectAllTodosLoaded))),
     filter(([action, allTodosLoaded]) => !allTodosLoaded),
-    mergeMap(() => this.todosService.findAllTodos()),
-    map(todos => new LoadTodosSuccess({todos}))
+    mergeMap(() => this.todosService.findAllTodos().pipe(
+      map((allTodos: Todo[]) => new LoadTodosSuccess({todos: allTodos})),
+      catchError(error => of(new LoadTodosFailure({error})))
+    ))
   );
+
+  @Effect()
+  updateTodo$: Observable<Action> = this.actions$.pipe(
+    ofType(TodoActionTypes.UpdateTodo),
+    mergeMap((action: UpdateTodo) => this.store.pipe(select(selectTodoById(+action.payload.todo.id)))),
+    switchMap((todo: Todo) => this.todosService.updateTodo(todo).pipe(
+      map((updatedTodo: Todo) => new UpdateTodoSuccess({todo: updatedTodo})),
+      catchError(error => of(new UpdateTodoFailure({error}))
+    ))
+  ));
 
   constructor(private actions$: Actions, private todosService: TodosService,
     private store: Store<AppState>) {}
