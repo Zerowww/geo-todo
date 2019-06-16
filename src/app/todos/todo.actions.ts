cb3b8335734adf4f39e7c12f5202f70a6af41ac7
zeroww <dev.zeroww@gmail.com>
2019-06-16 14:42:45 +0200

    feat(todo-actions): :sparkles: added updatetodo, success and failure
    undefined

--- src/app/todos/todo.actions.ts
+++ src/app/todos/todo.actions.ts
@@ -1,3 +1,4 @@
+import { Update } from '@ngrx/entity';
 import { Action } from '@ngrx/store';
 
 import { Todo } from '../shared/models/todo.model';
@@ -6,6 +7,10 @@ export enum TodoActionTypes {
   TodosRequested = '[Todo Page] Todos Requested',
   LoadTodosSuccess = '[Todo API] Load Todos Success',
   LoadTodosFailure = '[Todo API] Load Todos Failure',
+
+  UpdateTodo = '[Todo Page] Todo Update',
+  UpdateTodoSuccess = '[Todo API] Update Todo Success',
+  UpdateTodoFailure = '[Todo API] Update Todos Failure',
 }
 
 export class TodosRequested implements Action {
@@ -22,5 +27,20 @@ export class LoadTodosFailure implements Action {
   constructor(public payload: { error: any }) { }
 }
 
-export type TodoActions = TodosRequested | LoadTodosSuccess | LoadTodosFailure;
+export class UpdateTodo implements Action {
+  readonly type = TodoActionTypes.UpdateTodo;
+  constructor(public payload: { todo: Update<Todo> }) { }
+}
+
+export class UpdateTodoSuccess implements Action {
+  readonly type = TodoActionTypes.UpdateTodoSuccess;
+  constructor(public payload: { todo: Todo }) { }
+}
+
+export class UpdateTodoFailure implements Action {
+  readonly type = TodoActionTypes.UpdateTodoFailure;
+  constructor(public payload: { error: any }) { }
+}
+
+export type TodoActions = TodosRequested | LoadTodosSuccess | LoadTodosFailure | UpdateTodo | UpdateTodoSuccess | UpdateTodoFailure;
 
