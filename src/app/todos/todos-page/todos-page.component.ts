e70b34fe524bbb2e32a7be799f999bab44d7b0dd
zeroww <dev.zeroww@gmail.com>
2019-06-17 12:57:05 +0200

    feat(todos-page): :sparkles: added todo creation dialog
    undefined

--- src/app/todos/todos-page/todos-page.component.ts
+++ src/app/todos/todos-page/todos-page.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Todo } from 'src/app/shared/models/todo.model';
 
+import { TodoCreationDialogComponent } from '../todo-creation-dialog/todo-creation-dialog.component';
 import { TodoCollectionRequested } from '../todo.actions';
 import { TodosState } from '../todo.reducer';
 import { selectAllTodos } from '../todo.selectors';
@@ -15,11 +17,21 @@ import { selectAllTodos } from '../todo.selectors';
 export class TodosPageComponent implements OnInit {
   public allTodos$: Observable<Todo[]>;
 
-  constructor(private store: Store<TodosState>) {}
+  constructor(private store: Store<TodosState>, private dialog: MatDialog) {}
 
   ngOnInit() {
     this.store.dispatch(new TodoCollectionRequested());
 
     this.allTodos$ = this.store.pipe(select(selectAllTodos));
+  }
+
+  public createTodo(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+    dialogConfig.width = '400px';
+
+    const dialogRef = this.dialog.open(TodoCreationDialogComponent, dialogConfig);
   }
 }
--- src/app/todos/todos-page/todos-page.module.ts
+++ src/app/todos/todos-page/todos-page.module.ts
@@ -1,13 +1,16 @@
-import {NgModule} from '@angular/core';
-import {SharedModule} from 'src/app/shared/shared.module';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
-import {TodosListModule} from '../todos-list/todos-list.module';
-import {TodosPageComponent} from './todos-page.component';
-import {TodosPageRoutingModule} from './todos-page.routing';
+import { TodoCreationDialogComponent } from '../todo-creation-dialog/todo-creation-dialog.component';
+import { TodosListModule } from '../todos-list/todos-list.module';
+import { TodosPageComponent } from './todos-page.component';
+import { TodosPageRoutingModule } from './todos-page.routing';
 
 @NgModule({
-  declarations: [TodosPageComponent],
-  imports: [SharedModule, TodosPageRoutingModule, TodosListModule],
-  exports: [TodosPageComponent],
+  declarations: [TodosPageComponent, TodoCreationDialogComponent],
+  imports: [SharedModule, ReactiveFormsModule, TodosPageRoutingModule, TodosListModule],
+  exports: [TodosPageComponent, TodoCreationDialogComponent],
+  entryComponents: [TodoCreationDialogComponent],
 })
 export class TodosPageModule {}
