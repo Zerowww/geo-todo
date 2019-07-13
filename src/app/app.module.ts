a949bf50f66e9f94f72199ff874136f52d80df99
zeroww <dev.zeroww@gmail.com>
2019-07-13 21:45:24 +0200

    build: :package: updated project for firebase hosting
    undefined

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,38 +1,36 @@
-import {NgModule} from '@angular/core';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreModule} from '@ngrx/store';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import { NgModule } from '@angular/core';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 
-import {environment} from '../environments/environment';
-import {AppComponent} from './app.component';
-import {AppRoutingModule} from './app.routing';
-import {CoreModule} from './core/core.module';
-import {MockTodos} from './core/mock/mock-todos';
-import {metaReducers, reducers} from './reducers';
+import { environment } from '../environments/environment';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { CoreModule } from './core/core.module';
+import { MockTodos } from './core/mock/mock-todos';
+import { metaReducers, reducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
-    StoreModule.forRoot(reducers, {metaReducers}),
+    StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
     !environment.production
       ? StoreDevtoolsModule.instrument({
           name: 'NgRx Geo Store Devtools',
-          logOnly: environment.production,
+          logOnly: environment.production
         })
       : [],
-    environment.useMockData
-      ? HttpClientInMemoryWebApiModule.forRoot(MockTodos, {
-          delay: 1000,
-          post204: false,
-          put204: false,
-        })
-      : [],
-    CoreModule,
+    HttpClientInMemoryWebApiModule.forRoot(MockTodos, {
+      delay: 1000,
+      post204: false,
+      put204: false
+    }),
+    CoreModule
   ],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
